e6542377637fb9b8835112f62a4cd382b5a7c8b3
Lime|DF <53081076+LimeDF@users.noreply.github.com>
2021-02-10 02:18:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+jQuery(document).ready(function($){
+	$( ".logoT" ).click(function(){ $(".logoT1").html("Dніпровський")});
+		$( ".logoT" ).click(function(){ $(".logoT1").css({"left": "-280px"}); });
+	$( ".logoT" ).click(function(){ $(".logoT2").html("Dержавний")});
+		$( ".logoT" ).click(function(){ $(".logoT2").css({"left": "-150px"}); });
+	$( ".logoT" ).click(function(){ $(".logoT3").html("Tехнікум")});
+		$( ".logoT" ).click(function(){ $(".logoT3").css({"left": "-50px"}); });
+	$( ".logoT" ).click(function(){ $(".logoT4").html("Eнергетичних")});
+		$( ".logoT" ).click(function(){ $(".logoT4").css({"left": "65px"}); });
+	$( ".logoT" ).click(function(){ $(".logoT5").html("Iнформаційних")});
+		$( ".logoT" ).click(function(){ $(".logoT5").css({"left": "205px"}); });
+	$( ".logoT" ).click(function(){ $(".logoT6").html("Tехнологій")});
+		$( ".logoT" ).click(function(){ $(".logoT6").css({"left": "335px"}); });
+$( ".logoT" ).click(function(){ $(".material-icons").css({"transform": "rotate(0deg)", "color": "#FFFFFF"}); });
+});
